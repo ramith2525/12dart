e590b5867197636fddf858d4a81e956ac8c52232
remit <ramithp448@gmail.com>
2024-09-20 19:24:39 +0530

    commit

---
+++ bin/samplr.dart
@@ -0,0 +1,6 @@
+void main() }
+    print('welcome');
+print('22');
+print('vatakara');
+print('true');
+}
---
+++ bin/variables.dart
@@ -0,0 +1,14 @@
+void main() {
+  String name = "ramith";
+  print('My name is $name');
+  name = "manu";
+  print('My name is $name');
+  int age = 25;
+  print('my age is $age');
+  double mark = 25.5;
+  print('my mark is $mark');
+  int a = 20;
+  int b = 5;
+  int sum = a + b;
+  print('sum=$sum');
+}
